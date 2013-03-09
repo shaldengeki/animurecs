afcd9746d55ca1982464b02cbf02df20dcb71780
Shal Dengeki <shaldengeki@gmail.com>
2013-03-09 17:29:28 -0600

    minify, rendering optimizations

--- views/application/header.php
+++ views/application/header.php
@@ -7,32 +7,32 @@
 <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
   <head>
-    <meta http-equiv='content-type' content='text/html; charset=utf-8' />
+    <meta charset='utf-8' />
     <title><?php echo (isset($params['title']) ? escape_output($params['title']) : "Animurecs").($subtitle != '' ? ' - '.escape_output($subtitle) : ''); ?></title>
-    <link rel='shortcut icon' href='/favicon.ico' />
+    <link href='/favicon.ico' rel='shortcut icon' />
 
-    <link rel='stylesheet' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css' type='text/css' />
-    <link rel='stylesheet' href='<?php echo Config::ROOT_URL; ?>/css/jquery.dataTables.css' type='text/css' />
-    <link rel='stylesheet' href='<?php echo Config::ROOT_URL; ?>/css/token-input.css' type='text/css' />
-    <link rel='stylesheet' href='<?php echo Config::ROOT_URL; ?>/css/animurecs.css' type='text/css' />
+    <link href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css' rel='stylesheet' type='text/css' />
+    <link href='<?php echo Config::ROOT_URL; ?>/css/jquery.dataTables.css' rel='stylesheet' type='text/css' />
+    <link href='<?php echo Config::ROOT_URL; ?>/css/token-input.css' rel='stylesheet' type='text/css' />
+    <link href='<?php echo Config::ROOT_URL; ?>/css/animurecs.css' rel='stylesheet' type='text/css' />
 
-    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
-    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js'></script>
+    <script src='//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js' type='text/javascript'></script>
+    <script src='//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js' type='text/javascript'></script>
 
-    <!--<script type='text/javascript' src='<?php echo Config::ROOT_URL; ?>/jquery-ui-timepicker-addon.js'></script>-->
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/jquery.dropdownPlain.js'></script>
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/jquery.dataTables.min.js'></script>
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/jquery.tokeninput.js'></script>
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/jquery.json-2.3.min.js'></script>
+    <!--<script type='text/javascript' src='<?php echo Config::ROOT_URL; ?>/jquery-ui-timepicker-addon.min.js'></script>-->
+    <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.dropdownPlain.min.js' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.dataTables.min.js' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.tokeninput.min.js' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.json-2.3.min.js' type='text/javascript'></script>
 
-    <script type='text/javascript' src='https://www.google.com/jsapi'></script>
-    <script type='text/javascript' src='<?php echo Config::ROOT_URL; ?>/js/d3.v2.min.js'></script>
-    <script type='text/javascript' src='<?php echo Config::ROOT_URL; ?>/js/d3-helpers.js'></script>
+    <script src='https://www.google.com/jsapi' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/d3.v2.min.js' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/d3-helpers.js' type='text/javascript'></script>
 
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/bootstrap.min.js'></script>
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/bootstrap-dropdown.js'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/bootstrap.min.js' type='text/javascript'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/bootstrap-dropdown.min.js' type='text/javascript'></script>
 
-    <script type='text/javascript' language='javascript' src='<?php echo Config::ROOT_URL; ?>/js/animurecs.js'></script>
+    <script src='<?php echo Config::ROOT_URL; ?>/js/animurecs.js' type='text/javascript'></script>
     <script type="text/javascript">
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-37523517-1']);
