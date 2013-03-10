88abf34e43138e30d3777ce0c0676647baa2a4e4
Shal Dengeki <shaldengeki@gmail.com>
2013-03-09 18:17:00 -0600

    cdnjs, minify datatables

--- views/application/header.php
+++ views/application/header.php
@@ -7,11 +7,11 @@
 <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
   <head>
-    <meta charset='utf-8' />
+    <meta content='text/html; charset=UTF-8' http-equiv='content-type' />
     <title><?php echo (isset($params['title']) ? escape_output($params['title']) : "Animurecs").($subtitle != '' ? ' - '.escape_output($subtitle) : ''); ?></title>
     <link href='/favicon.ico' rel='shortcut icon' />
 
-    <link href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css' rel='stylesheet' type='text/css' />
+    <link href='//ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css' rel='stylesheet' type='text/css' />
     <link href='<?php echo Config::ROOT_URL; ?>/css/jquery.dataTables.css' rel='stylesheet' type='text/css' />
     <link href='<?php echo Config::ROOT_URL; ?>/css/token-input.css' rel='stylesheet' type='text/css' />
     <link href='<?php echo Config::ROOT_URL; ?>/css/animurecs.css' rel='stylesheet' type='text/css' />
@@ -21,15 +21,15 @@
 
     <!--<script type='text/javascript' src='<?php echo Config::ROOT_URL; ?>/jquery-ui-timepicker-addon.min.js'></script>-->
     <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.dropdownPlain.min.js' type='text/javascript'></script>
-    <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.dataTables.min.js' type='text/javascript'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js' type='text/javascript'></script>
     <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.tokeninput.min.js' type='text/javascript'></script>
     <script src='<?php echo Config::ROOT_URL; ?>/js/jquery.json-2.3.min.js' type='text/javascript'></script>
 
-    <script src='https://www.google.com/jsapi' type='text/javascript'></script>
-    <script src='<?php echo Config::ROOT_URL; ?>/js/d3.v2.min.js' type='text/javascript'></script>
+    <script src='//www.google.com/jsapi' type='text/javascript'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/d3/3.0.1/d3.v3.min.js' type='text/javascript'></script>
     <script src='<?php echo Config::ROOT_URL; ?>/js/d3-helpers.js' type='text/javascript'></script>
 
-    <script src='<?php echo Config::ROOT_URL; ?>/js/bootstrap.min.js' type='text/javascript'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js' type='text/javascript'></script>
     <script src='<?php echo Config::ROOT_URL; ?>/js/bootstrap-dropdown.min.js' type='text/javascript'></script>
 
     <script src='<?php echo Config::ROOT_URL; ?>/js/animurecs.js' type='text/javascript'></script>
