7cb6f04149c4b37fe41e56e1eb370f253a8d83ef
Shal Dengeki <shaldengeki@gmail.com>
2013-03-15 23:09:07 -0500

    redirect to globalFeed on login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("global/includes.php");
 if ($app->user->loggedIn()) {
-	redirect_to("feed.php", $_REQUEST);
+	redirect_to($app->user->url("globalFeed"), $_REQUEST);
 } else {
   echo $app->render($app->view('index'));
 }
