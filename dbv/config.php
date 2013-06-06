2db146399f36890dbe548de20d2768f07fcc3a51
Shal Dengeki <shaldengeki@gmail.com>
2013-06-06 11:37:06 -0500

    update dbv

---
+++ dbv/config.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Authentication data for access to DBV itself
+ * If you leave any of the two constants below blank, authentication will be disabled
+ * @see http://dbv.vizuina.com/documentation/#optional-settings
+ */
+define('DBV_USERNAME', Config::MYSQL_USERNAME);
+define('DBV_PASSWORD', Config::MYSQL_PASSWORD);
+
+/**
+ * @see http://dbv.vizuina.com/documentation/#writing-adapters
+ */
+define('DB_ADAPTER', 'MySQL');
+
+define('DS', DIRECTORY_SEPARATOR);
+define('DBV_ROOT_PATH', dirname(__FILE__));
+
+/**
+ * Only edit this lines if you want to place your schema files in custom locations
+ * @see http://dbv.vizuina.com/documentation/#optional-settings
+ */
+define('DBV_DATA_PATH', DBV_ROOT_PATH . DS . 'data');
+define('DBV_SCHEMA_PATH', DBV_DATA_PATH . DS . 'schema');
+define('DBV_REVISIONS_PATH', DBV_DATA_PATH . DS . 'revisions');
+define('DBV_META_PATH', DBV_DATA_PATH . DS . 'meta');
+
+
+ini_set('magic_quotes_gpc', 'Off');
+error_reporting(E_ALL ^ E_NOTICE);
+
+/**
+ * I18n support
+ */
+define('DBV_LANGUAGES_PATH', DBV_ROOT_PATH . DS . 'languages');
+define('DEFAULT_LOCALE', 'en_US');
+define('DEFAULT_ENCODING', 'UTF-8');
+define('DEFAULT_DOMAIN', 'default');
+
+putenv("LC_ALL=".DEFAULT_LOCALE);
+setlocale(LC_ALL, DEFAULT_LOCALE);
+
+bindtextdomain(DEFAULT_DOMAIN, DBV_LANGUAGES_PATH);
+bind_textdomain_codeset(DEFAULT_DOMAIN, DEFAULT_ENCODING);
+textdomain(DEFAULT_DOMAIN);
