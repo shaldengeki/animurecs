shaldengeki/animurecs

commit a8170096208f3c28044ecc8713f0a17d77004701

fix register bug; fix issue#42

js/animurecs.js
@@ -1,5 +1,9 @@
 var windowIntervals = [];
 
+// window scroll event throttling.
+var scrollThrottleDelay = 100;
+var scrollThrottleTimer = null;
+
 /* Default class modification */
 $.extend( $.fn.dataTableExt.oStdClasses, {
   "sWrapper": "dataTables_wrapper form-inline"
@@ -518,6 +522,43 @@ function updateFeedTimes() {
   });
 }
 
+function ScrollHandler(e) {
+  //throttle scroll event.
+  var elt = e.data.elt;
+  clearTimeout(scrollThrottleTimer);
+  scrollThrottleTimer = setTimeout(function () {
+    // check that we haven't scrolled to the bottom of an ajax feed.
+    $(elt).find('ul.ajaxFeed:visible').each(function() {
+      if ($(this).children('li').length > 0 && $(this).height() <= ($(window).height() + $(window).scrollTop()) && !$(this).hasAttr('loading')) {
+        var feedNode = this;
+        //get last-loaded list change and load more past this.
+        $(feedNode).attr('loading', 'true');
+        // find the lowest feedDate on the page.
+        var feedDates = $('.feedDate').map(function() {
+          return $(feedNode).attr('data-time');
+        }).get();
+        var lastTime = Array.min(feedDates);
+        var anime_id = $(feedNode).hasAttr('anime_id') ? $(feedNode).attr('anime_id') : "";
+        var user_id = $(feedNode).hasAttr('user_id') ? $(feedNode).attr('user_id') : "";
+        if ($(feedNode).attr('data-url').indexOf('?') < 0) {
+          joinChar = '?';
+        } else {
+          joinChar = '&';
+        }
+        $.ajax({
+          url: $(feedNode).attr('data-url') + joinChar + "maxTime=" + encodeURIComponent(lastTime) + ((anime_id != "" && !isNaN(parseInt(anime_id))) ? '&anime_id=' + parseInt(anime_id) : "") + ((user_id != "" && !isNaN(parseInt(user_id))) ? '&user_id=' + parseInt(user_id) : ""),
+          data: {},
+          success: function(data) {
+            $(feedNode).append($(data).html());
+            initInterface($(feedNode).parent());
+            $(feedNode).removeAttr('loading');
+          }
+        });
+      }
+    });
+  }, scrollThrottleDelay);  
+}
+
 function initInterface(elt) {
   // initializes all interface elements and events within a given element.
 
@@ -535,6 +576,9 @@ function initInterface(elt) {
          urlParams[decode(match[1])] = decode(match[2]);
   })();
 
+  // window scroll events.
+  $(window).off('scroll', ScrollHandler)
+    .on('scroll', {elt: elt}, ScrollHandler);
 
   $(elt).find('.dropdown-toggle').dropdown();
   $(elt).tooltip({
@@ -548,15 +592,17 @@ function initInterface(elt) {
   renderGraphs(elt);
 
   /* Disable buttons on click */
-  $(elt).find('.btn').each(function() {
-    $(this).click( function() {
-      $(this).off('click');
-      $(this).addClass("disabled");
-      if (!$(this).hasAttr('data-loading-text')) {
-        $(this).text("Loading...");
-      } else {
-        $(this).text($(this).attr('data-loading-text'));
-      }
+  $(elt).find('form').each(function() {
+    $(this).submit( function() {
+      $(this).children('input[type=submit], button').each(function() {
+        $(this).attr('disabled', 'disabled');
+        $(this).addClass("disabled");
+        if (!$(this).hasAttr('data-loading-text')) {
+          $(this).text("Loading...");
+        } else {
+          $(this).text($(this).attr('data-loading-text'));
+        }
+      });
     });
   });
 
@@ -655,40 +701,11 @@ function initInterface(elt) {
             updateTitleItems(data.match(/\<li/gi).length);
           }
           $(feedNode).prepend($(data).children('li'));
+          initInterface($(feedNode).parent());
         }
       });
       updateFeedTimes();
     }, 10000));
-  });
-  $(window).unbind("scroll");
-  $(window).scroll(function() {
-    $(elt).find('ul.ajaxFeed:visible').each(function() {
-      if ($(this).children('li').length > 0 && $(this).height() <= ($(window).height() + $(window).scrollTop()) && !$(this).hasAttr('loading')) {
-        //get last-loaded list change and load more past this.
-        $(this).attr('loading', 'true');
-        // find the lowest feedDate on the page.
-        var feedDates = $('.feedDate').map(function() {
-          return $(this).attr('data-time');
-        }).get();
-        var lastTime = Array.min(feedDates);
-        var anime_id = $(this).hasAttr('anime_id') ? $(this).attr('anime_id') : "";
-        var user_id = $(this).hasAttr('user_id') ? $(this).attr('user_id') : "";
-        if ($(this).attr('data-url').indexOf('?') < 0) {
-          joinChar = '?';
-        } else {
-          joinChar = '&';
-        }
-        var originalElt = this;
-        $.ajax({
-          url: $(this).attr('data-url') + joinChar + "maxTime=" + encodeURIComponent(lastTime) + ((anime_id != "" && !isNaN(parseInt(anime_id))) ? '&anime_id=' + parseInt(anime_id) : "") + ((user_id != "" && !isNaN(parseInt(user_id))) ? '&user_id=' + parseInt(user_id) : ""),
-          data: {},
-          success: function(data) {
-            $(originalElt).append($(data).html());
-            $(originalElt).removeAttr('loading');
-          }
-        });
-      }
-    });
   });
 
   /* ajax tab autoloading. */

global/application.php
@@ -309,7 +309,7 @@ class Application {
 
     // user stats metrics.
     $this->bind(['User.afterCreate', 'User.afterDelete'], new Observer(function($event, $parent, $updateParams) {
-      $parent->app->statsd->gauge("user.count", $parent->app->dbConn->count("SELECT COUNT(*) FROM `users`"));
+      $parent->app->statsd->gauge("user.count", User::count($parent->app));
     }));
     $this->bind(['User.logIn'], new Observer(function($event, $parent, $updateParams) {
       $parent->app->statsd->increment("user.login");

global/models/anime.php
@@ -86,7 +86,6 @@ class Anime extends BaseObject {
         }
         return False;
         break;
-      case 'token_search':
       case 'new':
         if ($authingUser->loggedIn()) {
           return True;
@@ -102,6 +101,7 @@ class Anime extends BaseObject {
         }
         return False;
         break;
+      case 'token_search':
       case 'index':
         return True;
         break;

commit 119b1470bcc45d963c31fc16b06a5d2e4d2fa8de

fix ajaxFeed regression in a8170096208f3c28044ecc8713f0a17d77004701

js/animurecs.js
@@ -534,8 +534,8 @@ function ScrollHandler(e) {
         //get last-loaded list change and load more past this.
         $(feedNode).attr('loading', 'true');
         // find the lowest feedDate on the page.
-        var feedDates = $('.feedDate').map(function() {
-          return $(feedNode).attr('data-time');
+        var feedDates = $(feedNode).find('.feedDate').map(function() {
+          return $(this).attr('data-time');
         }).get();
         var lastTime = Array.min(feedDates);
         var anime_id = $(feedNode).hasAttr('anime_id') ? $(feedNode).attr('anime_id') : "";

views/application/header.php
@@ -3,7 +3,7 @@
     echo "This partial cannot be rendered by itself.";
     exit;
   }
-  $assetsVersion = 0.73;
+  $assetsVersion = 0.74;
   $firstAnime = class_exists("Anime") ? Anime::first($this) : Null;
   $params['container'] = isset($params['container']) ? $params['container'] : True;
   $params['title'] = isset($params['title']) ? $params['title'] : "Animurecs";