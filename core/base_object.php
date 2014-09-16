e5639d00cff666a6341001d869e4202a5e0bd272
Charles Guo <shaldengeki@gmail.com>
2014-09-16 16:27:45 -0700

    fix typo.

--- core/base_object.php
+++ core/base_object.php
@@ -325,10 +325,10 @@ abstract class BaseObject {
         }
       }
     } else {
-      // attempt to retrieve ths object from the cache.
+      // attempt to retrieve this object from the cache.
       $cas = "";
       $info = $this->app->cache->get($this->cacheKey(), $foo, $cas);
-      if ($this->app->cache->resultCode() && $this->app->cache->resultCode() !== Memcached::RES_NOTFOUND) {
+      if ($this->app->cache->resultCode() !== Memcached::RES_NOTFOUND) {
         $this->set($info);
         $this->app->dbConn->reset();
         return $this;
