27e506042649d2aee921d418666f937c96ed372c
Charles Guo <shaldengeki@gmail.com>
2014-10-16 17:34:15 -0500

    Application: include resource in 404

--- application.php
+++ application.php
@@ -650,7 +650,7 @@ class Application {
     }
 
     if (!isset($this->controller) || $this->controller === "") {
-      $this->display_error(404, "The resource you requested does not exist.");
+      $this->display_error(404, "The resource you requested (".$_REQUEST['controller'].") does not exist.");
     }
 
     try {
