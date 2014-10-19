7bb157b681aa780c286a14efec835b4ed62cecb7
Charles Guo <shaldengeki@gmail.com>
2014-10-18 20:59:23 -0500

    define getallheaders() if necessary

--- core/misc.php
+++ core/misc.php
@@ -1,4 +1,16 @@
 <?php
+if (!function_exists('getallheaders')) {
+  function getallheaders() {
+    $headers = '';
+    foreach ($_SERVER as $name => $value) {
+      if (substr($name, 0, 5) == 'HTTP_') {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
+}
+
 function is_integral($value) {
   // returns bool reflecting whether the current value is integral.
   return is_numeric($value) && intval($value) == $value;
